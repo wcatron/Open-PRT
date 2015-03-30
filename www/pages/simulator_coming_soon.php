de5785748e0f332307fbde0937909e28f9b13204
Weston Catron <wcatron@catrondevelopment.com>
2015-03-30 15:17:52 -0400

    setup link for simluator coming soon

---
+++ www/pages/simulator_coming_soon.php
@@ -0,0 +1,6 @@
+<h2>Simulator</h2>
+<p>We think that accessing information about PRT is important and we're designing a simulator that will be able to run in your browser.</p>
+
+<p><h4>Coming Soon!</h4></p>
+
+<p>If you'd like to help develop this please <a href="mailto:wcatron@catrondevelopment.com">contact us</a>!</p>
