726dc9893b82f9c43740955663afbae8d57bac68
Weston Catron <wcatron@catrondevelopment.com>
2015-03-30 14:51:02 -0400

    setup about news and simulator

--- www/pages/about.php
+++ www/pages/about.php
@@ -1,1 +1,9 @@
-<h1>About</h1>
+<h2>About</h2>
+<p>Open source documentation and protocols for a scalable Personal Rapid Transit (PRT, aka: PodCar) system.</p>
+
+<p><b>Vision</b></p>
+
+<p>To create a standard for communciation between riders, podcars and decenteralized servers to maintain a PRT system. Our inspiration comes from growth of the internet due to standards, openness and healthy capitalism. We believe that any successful system will come about through a community effort and not by a single governing body, company or even a select few. </p>
+
+<p><h4>Who is Open PRT?</h4></p>
+<p>Open PRT is an advocate for PRT and seeks to offer a solution through standardization and partnerships with industry leaders and interested parties. We are focused on creating a standard for PRT installations that will allow for growth and innovation for the next century. To be clear there currently is not a singular Open PRT design but we are seeking interested partners to contribute to creating a design that could ultimately be implemented in Charleston and around the world.</p>
---
+++ www/pages/news.php
@@ -0,0 +1,6 @@
+<h2>News</h2>
+<p>Find out what's going on with the Open PRT initiative.</p>
+
+<p><h4>Update March 30th, 2015</h4></p>
+
+<p>Reaching out to partners in the area and gauging feedback on ideas around modularity and openness.</p>
